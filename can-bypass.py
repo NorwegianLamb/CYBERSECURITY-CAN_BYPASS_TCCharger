88dc92bf438a36bf04fe5256afa65491bb4feb44
NorwegianLamb <norwegianlamb@protonmail.com>
2023-09-20 23:32:55 +0200

    quick fixes
    added ARGV parser, setting up the code in order to be configured by the user

--- can-bypass.py
+++ can-bypass.py
@@ -1,21 +1,16 @@
 """
 Author: Flavio Gjoni - Electric QDL
 Description: BMS-CAN emulator to deploy 100V/60A of charge
-Version: 1.0x
+Version: 1.1x
 """
 import time
 import can
+import argparse
 
-# ID gathered from DOCs
-charger_id = 0x18FF50E5
+# Setting the bmsID, CAN channel and bustype (bitrate is default 250k)
 bms_id = 0x1806E5F4
-# CAN bus interface
 bus = can.interface.Bus(channel='can0', bustype='socketcan', bitrate=250000)
-# define data, to choose in sendMessage param? (so we can switch signals)
-message_data_heat = bytearray([0x03, 0xE8, 0x02, 0x58, 0x00, 0x01, 0x00, 0x00])
-message_data_charge = bytearray([0x03, 0xE8, 0x02, 0x58, 0x00, 0x00, 0x00, 0x00])
-temp_heat = bytearray([0x03,0xE8,0x02,0x58,0x00,0x01,0x00,0x00])
-message = can.Message(arbitration_id=bms_id, data=message_data_charge, is_extended_id=True)
+message_data = bytearray([0x03, 0xE8, 0x02, 0x58, 0x00, 0x01, 0x00, 0x00]) #0x01 for HEATING MODE
 
 #heat enable first, then disable:
 def enableHeat():
@@ -33,8 +28,7 @@ def enableHeat():
 
 # add params to define message types? (if automated charger)
 def sendMessage():
-	enableHeat()
-	time.sleep(3)
+
 	i = 0
 	try:
 		while (i < 15):
@@ -49,7 +43,12 @@ def sendMessage():
 
 # entry point from running command?
 if __name__ == "__main__":
-	print("\n")
-	sendMessage()
+	parser = argparse.ArgumentParser(description="Automated scraper for the SISTER framework, it gathers the PDF files for property successions and parses them")
+	parser.add_argument("-V", "--voltage", required=True, help="Select the voltage required, eg. '-V 100.0'. (DEFAULT: 100.0V)")
+	parser.add_argument("-A", "--amperage", required=True, help="Select the amperage required, eg. '-A 60.0'. (DEFAULT: 60.0A)")
+	parser.add_argument("-m", "--mode", required=True, help="Use '-m h' for heating mode or '-m c' for charging mode. (DEFAULT: Heating mode)")
+	parser.add_argument("-t", "--time", required=True, help="Choose for how much time (in seconds) to keep the charger active. (DEFAULT: 300s=5m)")
+	args = parser.parse_args()
+	sendMessage(args.voltage, args.amperage, args.mode, args.time)
 	bus.shutdown()
-	print("\nExiting code...")
+	print("\nBus in shutdown, closing the program...")
